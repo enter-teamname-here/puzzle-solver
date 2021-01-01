7d3a651a879ebfacab884ea1c68d6dc612bafe4c
Ryang Sohn <loop.infinitely@gmail.com>
2021-01-02 07:08:38 +0900

    Add unit tests

---
+++ test/src/puzzlelib_tests.cc
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <puzzle_def.h>
+
+#include <utility>
+
+using puzzlelib::Puzzle;
+using std::make_pair;
+
+class PuzzlelibTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {}
+  virtual void TearDown() {}
+  virtual void verify_dim(int grid_width, int grid_height) {
+    Puzzle puzzle(grid_width, grid_height);
+    ASSERT_EQ(puzzle.grid_dimension(), make_pair(grid_width, grid_height));
+  }
+};
+
+TEST_F(PuzzlelibTest, 9_by_9) { verify_dim(9, 9); }
