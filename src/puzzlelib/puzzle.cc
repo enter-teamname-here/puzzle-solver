6df855d3477d58c4da3568e72564b83825524382
Ryang Sohn <loop.infinitely@gmail.com>
2021-01-04 23:34:12 +0900

    Add rules and cell_contents to Puzzle class

--- src/puzzlelib/puzzle.cc
+++ src/puzzlelib/puzzle.cc
@@ -1,8 +1,18 @@
 #include "puzzle.h"
 
 namespace puzzlelib {
+Puzzle::Puzzle(int _grid_width, int _grid_height, std::vector<Rule> _rules,
+               std::vector<int> _cell_contents)
+    : grid_width(_grid_width),
+      grid_height(_grid_height),
+      rules(_rules),
+      cell_contents(_cell_contents){};
+
+Puzzle::Puzzle(int _grid_width, int _grid_height, std::vector<Rule> _rules)
+    : Puzzle(_grid_width, _grid_height, {}, {}) {}
+
 Puzzle::Puzzle(int _grid_width, int _grid_height)
-    : grid_width(_grid_width), grid_height(_grid_height) {}
+    : Puzzle(_grid_width, _grid_height, {}) {}
 
 std::pair<int, int> Puzzle::GetGridDimension() {
   return {this->grid_width, this->grid_height};
--- src/puzzlelib/puzzle.h
+++ src/puzzlelib/puzzle.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <utility>
+#include <vector>
 
 #include "rule.h"
 
@@ -9,8 +10,13 @@ class Puzzle {
  private:
   int grid_width;
   int grid_height;
+  std::vector<Rule> rules;
+  std::vector<int> cell_contents;
 
  public:
+  Puzzle(int _grid_width, int _grid_height, std::vector<Rule> _rules,
+         std::vector<int> _cell_contents);
+  Puzzle(int _grid_width, int _grid_height, std::vector<Rule> _rules);
   Puzzle(int _grid_width, int _grid_height);
   std::pair<int, int> GetGridDimension();
 };
